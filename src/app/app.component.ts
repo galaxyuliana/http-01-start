6bcd4e47bf79eaa0b9726315a851a46b89c2a7cd
galaxyuliana <yulianashin613@gmail.com>
2022-12-27 22:41:47 -0800

    Sent a POST request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,10 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post('https://ng-complete-guide-b78b6-default-rtdb.firebaseio.com/posts.json', postData)
+    .subscribe(responseData => {
+      console.log(responseData)
+    });
   }
 
   onFetchPosts() {
