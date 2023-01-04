698114cec20d7b76d76996f78f029dd9b67161a6
galaxyuliana <yulianashin613@gmail.com>
2023-01-03 20:26:39 -0800

    Added PostsService for HttpRequests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
     this.fetchPosts();
@@ -20,10 +21,7 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: Post) {
     // Send Http request
-    this.http.post<{ name: string }>('https://ng-complete-guide-b78b6-default-rtdb.firebaseio.com/posts.json', postData)
-    .subscribe(responseData => {
-      console.log(responseData)
-    });
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
---
+++ src/app/posts.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+
+    constructor(private http: HttpClient) {}
+
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = {title: title, content: content}
+        this.http.post<{ name: string }>('https://ng-complete-guide-b78b6-default-rtdb.firebaseio.com/posts.json', postData)
+        .subscribe(responseData => {
+          console.log(responseData)
+        });
+    }
+
+    fetchPosts() {
+
+    }
+}
