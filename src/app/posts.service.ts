560ce8c9bb6bec2572357f5e8e12486d0d97dddd
galaxyuliana <yulianashin613@gmail.com>
2023-01-11 21:01:36 -0800

    Set headers

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
 import { catchError, map } from "rxjs/operators";
@@ -22,7 +22,10 @@ export class PostsService {
 
     fetchPosts() {
       return this.http
-      .get<{ [key:string] : Post }>('https://ng-complete-guide-b78b6-default-rtdb.firebaseio.com/posts.json')
+      .get<{ [key:string] : Post }>('https://ng-complete-guide-b78b6-default-rtdb.firebaseio.com/posts.json',
+      {
+        headers: new HttpHeaders({'Custome-Header': 'Hello'})
+      })
       .pipe(map(responseData => {
         const postsArray: Post[] = [];
         for(const key in responseData) {
