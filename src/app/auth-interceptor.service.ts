806c58c64ed7954b3f586187ecd528fc579796b1
galaxyuliana <yulianashin613@gmail.com>
2023-01-20 23:12:33 -0800

    Multiple interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -6,15 +6,8 @@ export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // req: request object
         // next: function which will forward the request
-        console.log('Request is on its way');
         // const modifiedRequest = req.clone({url: 'new URL'});
         const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
-        return next.handle(modifiedRequest).pipe(tap(event => {
-            console.log(event);
-            if(event.type === HttpEventType.Response) {
-                console.log('Response arrived, body data: ');
-                console.log(event.body);
-            }
-        }));
+        return next.handle(modifiedRequest);
     }
 }
